1f63d197b8a79aa850ce7300f52dde5d6b48e697
Jonathan Cole <jon@jcole.io>
2023-10-08 10:55:54 -0400

    init, verified

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const [_a, _b, source, dest] = process.argv
+
+if (source === undefined || dest === undefined) process.exit(1)
+
+const regex = /^(vzdump-(?:lxc|qemu))-(\d+)-(\d{4}_\d{2}_\d{2}-\d{2}_\d{2}_\d{2})\.(.+)$/
+
+const files = fs.readdirSync(source)
+
+type HashResult = {[k: string]: {dateString: string, files: {original: string, link: string}[]}};
+
+const hash: HashResult = files.reduce((agg, f) => {
+	// console.log(f)
+	const result = regex.exec(f)
+	if (!result) process.exit(1)
+	// console.log(result)
+	const [filename, prefix, vm, dateString, extension] = result
+	const link = `${prefix}-${vm}.${extension}`
+	const fileObj = {
+		original: filename,
+		link: link
+	}
+
+	if (
+		agg[vm] === undefined || 
+		agg[vm].dateString < dateString
+	) {
+		agg[vm] = {
+			dateString,
+			files: [fileObj]
+		}
+	} else if (agg[vm].dateString === dateString) {
+		agg[vm].files = [...agg[vm].files, fileObj]
+	}
+
+	return agg;
+}, {} as HashResult)
+
+Object.keys(hash).forEach(vm => {
+	// console.log(vm, hash[vm].files)
+	hash[vm].files.forEach(f => fs.linkSync(
+		path.join(source, f.original),
+		path.join(dest, f.link)
+	))
+})
